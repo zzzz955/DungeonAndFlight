4ddc537bf645739a8c8d36f7c89da1fa31abbe3b
Sang <zzzzz955@gmail.com>
2024-05-08 17:33:12 +0900

    배경 완료
    배경 완료

--- DungeonAndFlight/Assets/Scripts/BackGround.cs
+++ DungeonAndFlight/Assets/Scripts/BackGround.cs
@@ -7,17 +7,23 @@ public class BackGround : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 0.5f;
-    [SerializeField]
-    private GameObject[] backGrounds;
-    public int backGroundIndex = 0;
-    private SpriteRenderer spriteRenderer;
     private float stopPositionX;
+    private float spwanPositionX;
     private bool isMoving = true;
+    private Camera mainCamera;
+    private float initialCameraHeight;
+    private Vector3 initialScales;
 
-    // Start is called before the first frame update
+    void Awake() {
+        mainCamera = Camera.main;
+        initialCameraHeight = 2f * mainCamera.orthographicSize;
+    }
+
     void Start()
     {
-        UpdateBackGround(backGroundIndex);
+        initialScales = transform.localScale;
+        ScaleBackground();        
+        transform.position = new Vector3(spwanPositionX, transform.position.y, transform.position.z);        
     }
 
     // Update is called once per frame
@@ -32,14 +38,25 @@ public class BackGround : MonoBehaviour
         }
     }
 
-    public void UpdateBackGround(int index) {
-        SizeCheck();
-        
-    }
+    void ScaleBackground()
+    {
+        // 배경 객체의 SpriteRenderer 컴포넌트 가져오기
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
 
-    void SizeCheck() {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        Vector2 spriteSize = spriteRenderer.bounds.size;
-        stopPositionX = -((spriteSize.x / 2f) - 9f);
-    }
+        if (spriteRenderer != null)
+        {
+            // 현재 배경의 스케일 계산
+            float currentHeight = spriteRenderer.bounds.size.y;
+            float scaleRatio = initialCameraHeight / currentHeight;
+
+            // 배경의 스케일 조정
+            transform.localScale = new Vector3(scaleRatio, scaleRatio, 1f);
+
+            // 스케일 조정 후의 배경의 너비와 높이 계산
+            float scaledWidth = spriteRenderer.bounds.size.x;
+
+            stopPositionX = -((scaledWidth / 2f) - 9f);
+            spwanPositionX = scaledWidth / 2f - 9f;
+        }
+    }    
 }
---
+++ DungeonAndFlight/Assets/Scripts/BackGroundController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class BackGroundController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] backGrounds;
+    public int backGroundIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowBackGround(backGroundIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowBackGround(int index) {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+        Instantiate(backGrounds[index], transform.position, Quaternion.identity);
+    }
+}
